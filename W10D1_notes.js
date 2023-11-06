c2e613781450036fb5c3e470eaeed6ce7dcd72e2
Anthony Lovern <voltorodarkwatch@gmail.com>
2023-11-06 17:20:10 -0500

    Through route handlers lecture

---
+++ W10D1_notes.js
@@ -0,0 +1,50 @@
+/*
+
+Frontend
+    UI/UX, fetching (requests), and requesting data from the server
+    Develop requests, handle responses
+
+Backend
+    Server sending data to client, data storage/manipulate, routing, security, DB
+    Handles requests, develops responses
+
+What is an API?
+    A tool for delivering data, creating a way for a user to interact with data
+
+What is a Framework?
+    Structure around which we build something else
+
+What is Express?
+    An API Framework
+    A backend is just a framework or skeleton that a project/site is built around
+
+Steps to start with Express
+    Initialize node
+        npm init -y
+    Install Packages
+        Express - npm install express
+        Nodemon - npm install -D nodemon
+
+    app.js - Main hub of our application
+        Import Express
+        Use express to instantiate app obj
+
+Route Handlers 
+    App object methods:
+        get, post, put/patch, delete, all, use
+    Request Path
+        String, array of strings, regular expressions, array of RegEx, array of strings and RegEx
+    Middleware
+        Series of callback functions
+    Response Methods
+        send (plain text or JSON)
+        json (always sends JSON)
+    app.<method>(<path>, (<middleware>) => { <response> })
+
+Our first Global Middleware
+    express.json() => Allow us to parse request body
+
+!! Express works top => down !!
+
+
+*/
