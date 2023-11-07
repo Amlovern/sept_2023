545af9bdee57a2389e4e65f70fd045536bbfd0f6
Anthony Lovern <voltorodarkwatch@gmail.com>
2023-11-07 12:20:58 -0500

    Through initial middleware lecture

--- demo_code/app.js
+++ demo_code/app.js
@@ -3,12 +3,38 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`initial path: ${req.path}`);
+    next()
+});
+
+app.use((req, res, next) => {
+    console.log('error test');
+    const error = 'There was an error :(';
+    next(error)
+})
+
+const nameWelcome = (req, res, next) => {
+    console.log('Welcome to Name endpoints');
+    console.log(`path inside nameWelcome: ${req.path}`);
+    next()
+};
+
+app.use('/name', nameWelcome);
+
+const checkUserInput = (req, res, next) => {
+    if (!req.body.stuff) {
+        return res.send('Please include a stuff property.')
+    }
+    next()
+}
+
 app.get(['/test', '/status'], (req, res) => {
     res.send('Hello from npm start')
 });
 
-app.post('/create', (request, response) => {
-    console.log(request.body)
+app.post('/create', checkUserInput, (request, response) => {
+    response.json(request.body)
 });
 
 app.get('/name/:artistId', (req, res) => {
@@ -24,6 +50,12 @@ app.get('/name', (req, res) => {
     const { actorName } = req.query;
 
     res.send(`The actor's name is ${actorName}`)
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500);
+    res.send(err)
 })
 
 app.listen(8000, () => console.log('Listening on port 8000...'))
