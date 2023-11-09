058786de18d1129f2a9cdf5c67b0827b547743ec
Anthony Lovern <voltorodarkwatch@gmail.com>
2023-11-09 13:22:40 -0500

    Through ON DELETE lecture

--- demo_code/mythology.sql
+++ demo_code/mythology.sql
@@ -1,27 +1,9 @@
+PRAGMA foreign_keys = 1;
+DROP TABLE IF EXISTS hero_abilities;
+DROP TABLE IF EXISTS abilities;
 DROP TABLE IF EXISTS heroes;
-
-CREATE TABLE heroes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR NOT NULL UNIQUE,
-    deity_status_id INTEGER REFERENCES deity_status(id),
-    famous_feat TEXT UNIQUE,
-    region_of_origin VARCHAR(75) NOT NULL,
-    ability TEXT,
-    weakness VARCHAR(125),
-    mortal_enemy VARCHAR NOT NULL,
-    year_of_origin INTEGER NOT NULL
-    -- FOREIGN KEY (deity_status_id) REFERENCES deity_status(id)
-);
-
-INSERT INTO heroes (name, deity_status_id, famous_feat, region_of_origin, ability, weakness, mortal_enemy, year_of_origin)
-VALUES
-('Hercules', 2, 'defeated the hydra', 'Rome', 'super strength', 'temper', 'Antaeus', -1286),
-('Thor', 1, 'lifted Mjolnir', 'Norse', 'lightning', 'certain magics', 'Loki', 50),
-('Horus', 1, 'became a pharaoh of Egypt', 'Egypt', 'transformation', 'women', 'Set', -2925),
-('King Arthur', 3, 'pulled Excalibur from the stone', 'British Isles', 'Excalibur', 'glory hungry', 'Mordred', 475),
-('Beowulf', 3, 'killed Grendel', 'Norse', 'super strength', 'arrogance', 'Grendel', 1000),
-('Amaterasu', 1, 'created the imperial regalia', 'Japan', 'brings light to the world', 'none', 'Arahabaki', 712),
-('Achilles', 3, 'slaying the trojan prince', 'Greece', 'super strength', 'bad ankle', 'Paris', -400);
+DROP TABLE IF EXISTS famous_feats;
+DROP TABLE IF EXISTS deity_status;
 
 CREATE TABLE deity_status ( -- one to many
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,26 +16,7 @@ VALUES
 ('demi-god'),
 ('human');
 
-
-
-
-
-
-CREATE TABLE abilities (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR NOT NULL UNIQUE
-);
-
-INSERT INTO abilities (name)
-VALUES
-('super strength'),
-('lightning'),
-('transformation'),
-('invisibility'),
-('brings light to the world'),
-('Excalibur');
-
-CREATE TABLE famous_feats (
+CREATE TABLE famous_feats ( -- one to one
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     feat TEXT NOT NULL
 );
@@ -68,3 +31,59 @@ VALUES
 ('defeated the hydra'),
 ('pulled Excalibur from the stone'),
 ('killed Grendel');
+
+CREATE TABLE heroes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL UNIQUE,
+    deity_status_id INTEGER REFERENCES deity_status(id) ON DELETE SET NULL,
+    famous_feat_id INTEGER NOT NULL UNIQUE,
+    region_of_origin VARCHAR(75) NOT NULL,
+    weakness VARCHAR(125),
+    mortal_enemy VARCHAR NOT NULL,
+    year_of_origin INTEGER NOT NULL,
+    FOREIGN KEY (famous_feat_id) REFERENCES famous_feats(id) ON DELETE CASCADE
+);
+
+INSERT INTO heroes (name, deity_status_id, famous_feat_id, region_of_origin, weakness, mortal_enemy, year_of_origin)
+VALUES
+('Hercules', 2, 6, 'Rome', 'temper', 'Antaeus', -1286),
+('Thor', 1, 3, 'Norse', 'certain magics', 'Loki', 50),
+('Horus', 1, 5, 'Egypt', 'women', 'Set', -2925),
+('King Arthur', 3, 7, 'British Isles', 'glory hungry', 'Mordred', 475),
+('Beowulf', 3, 8, 'Norse', 'arrogance', 'Grendel', 1000),
+('Amaterasu', 1, 1, 'Japan', 'none', 'Arahabaki', 712),
+('Achilles', 3, 4, 'Greece', 'bad ankle', 'Paris', -400);
+
+
+CREATE TABLE abilities ( -- many to many
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL UNIQUE
+);
+
+INSERT INTO abilities (name)
+VALUES
+('super strength'),
+('lightning'),
+('transformation'),
+('invisibility'),
+('brings light to the world'),
+('Excalibur');
+
+CREATE TABLE hero_abilities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    hero_id INTEGER NOT NULL REFERENCES heroes(id) ON DELETE CASCADE,
+    ability_id INTEGER NOT NULL REFERENCES abilities(id)
+);
+
+INSERT INTO hero_abilities (hero_id, ability_id)
+VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 6),
+(5, 1),
+(6, 5),
+(7, 1),
+(2, 1),
+(5, 4),
+(3, 2);
--- instructor_practices/W10/D4/2-sql-create-related-tables/sql-practice/music.sql
+++ instructor_practices/W10/D4/2-sql-create-related-tables/sql-practice/music.sql
@@ -1,5 +1,6 @@
 -- This ensures that SQLite enforces FOREIGN KEY constraints
 PRAGMA foreign_keys = 1;
+DROP TABLE IF EXISTS musician_instruments;
 DROP TABLE IF EXISTS instruments;
 DROP TABLE IF EXISTS musicians;
 DROP TABLE IF EXISTS bands;
@@ -11,9 +12,17 @@ CREATE TABLE bands (
 CREATE TABLE musicians (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100)
+  last_name VARCHAR(100),
+  bands_id INTEGER REFERENCES bands(id)
 );
 CREATE TABLE instruments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   type VARCHAR(100) NOT NULL
+);
+CREATE TABLE musician_instruments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  musician_id INTEGER,
+  instrument_id INTEGER,
+  FOREIGN KEY (musician_id) REFERENCES musicians(id),
+  FOREIGN KEY (instrument_id) REFERENCES instruments(id)
 );
