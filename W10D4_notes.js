c6e566054e7b5a8e78689d616bdfe119df7d6e5d
Anthony Lovern <voltorodarkwatch@gmail.com>
2023-11-09 11:54:34 -0500

    Through setting up FK

---
+++ W10D4_notes.js
@@ -0,0 +1,22 @@
+/*
+
+Relationships!
+
+What are the 3 types of relationships?
+    One to one -> will see the least
+    One to many
+    Many to many
+        Requires a Join table
+        A join table can have more than just the 2 foreign keys
+
+    In a one to many relationship, which side does the FK go on?
+        The many side!
+
+There are a couple ways to establish a FK in a CREATE TABLE
+    At the bottom of the CREATE command:
+        FOREIGN KEY (<FK col name>) REFERENCES <other table>(<pk>)
+    We can combine into the column
+        <col name> INTEGER REFERENCES <other table>(<pk>)
+
+
+*/
--- demo_code/mythology.sql
+++ demo_code/mythology.sql
@@ -3,19 +3,68 @@ DROP TABLE IF EXISTS heroes;
 CREATE TABLE heroes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR NOT NULL UNIQUE,
-    deity_status VARCHAR(8),
+    deity_status_id INTEGER REFERENCES deity_status(id),
     famous_feat TEXT UNIQUE,
     region_of_origin VARCHAR(75) NOT NULL,
     ability TEXT,
     weakness VARCHAR(125),
     mortal_enemy VARCHAR NOT NULL,
     year_of_origin INTEGER NOT NULL
+    -- FOREIGN KEY (deity_status_id) REFERENCES deity_status(id)
 );
 
-INSERT INTO heroes (name, deity_status, famous_feat, region_of_origin, ability, weakness, mortal_enemy, year_of_origin)
+INSERT INTO heroes (name, deity_status_id, famous_feat, region_of_origin, ability, weakness, mortal_enemy, year_of_origin)
 VALUES
-('Hercules', 'demi-god', 'defeated the hydra', 'Rome', 'super strength', 'temper', 'Antaeus', -1286),
-('Thor', 'god', 'lifted Mjolnir', 'Norse', 'lightning', 'certain magics', 'Loki', 50),
-('Horus', 'god', 'became a pharaoh of Egypt', 'Egypt', 'transformation', 'women', 'Set', -2925),
-('King Arthur', 'human', 'pulled Excalibur from the stone', 'British Isles', 'Excalibur', 'glory hungry', 'Mordred', 475),
-('Amaterasu', 'god', 'created the imperial regalia', 'Japan', 'brings light to the world', 'none', 'Arahabaki', 712);
+('Hercules', 2, 'defeated the hydra', 'Rome', 'super strength', 'temper', 'Antaeus', -1286),
+('Thor', 1, 'lifted Mjolnir', 'Norse', 'lightning', 'certain magics', 'Loki', 50),
+('Horus', 1, 'became a pharaoh of Egypt', 'Egypt', 'transformation', 'women', 'Set', -2925),
+('King Arthur', 3, 'pulled Excalibur from the stone', 'British Isles', 'Excalibur', 'glory hungry', 'Mordred', 475),
+('Beowulf', 3, 'killed Grendel', 'Norse', 'super strength', 'arrogance', 'Grendel', 1000),
+('Amaterasu', 1, 'created the imperial regalia', 'Japan', 'brings light to the world', 'none', 'Arahabaki', 712),
+('Achilles', 3, 'slaying the trojan prince', 'Greece', 'super strength', 'bad ankle', 'Paris', -400);
+
+CREATE TABLE deity_status ( -- one to many
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(8) NOT NULL UNIQUE
+);
+
+INSERT INTO deity_status (name)
+VALUES
+('god'),
+('demi-god'),
+('human');
+
+
+
+
+
+
+CREATE TABLE abilities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL UNIQUE
+);
+
+INSERT INTO abilities (name)
+VALUES
+('super strength'),
+('lightning'),
+('transformation'),
+('invisibility'),
+('brings light to the world'),
+('Excalibur');
+
+CREATE TABLE famous_feats (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    feat TEXT NOT NULL
+);
+
+INSERT INTO famous_feats (feat)
+VALUES
+('created the imperial regalia'),
+('gave fire to humans'),
+('lifted Mjolnir'),
+('slaying the trojan prince'),
+('became a pharaoh of Egypt'),
+('defeated the hydra'),
+('pulled Excalibur from the stone'),
+('killed Grendel');
